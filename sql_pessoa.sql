20ea4e38e9d73bfd936b991b6a697a2fed6e874a
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2021-10-22 20:53:34 -0400

    Commit
    Commit

---
+++ sql_pessoa.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `pessoa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_fantasia` varchar(150) DEFAULT NULL,
+  `sobrenome_razao` varchar(120) DEFAULT NULL,
+  `cpf_cnpj` varchar(30) DEFAULT NULL,
+  `rg_ie` varchar(45) DEFAULT NULL,
+  `nascimento_fundacao` date DEFAULT NULL,
+  `data_cadastro` timestamp NULL DEFAULT NULL,
+  `data_atualizacao` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
